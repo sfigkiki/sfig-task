8a86a87df3ac7f5c02e3085cb7cea79a62700b2f
sfigkiki <65834828+sfigkiki@users.noreply.github.com>
2025-10-04 00:20:25 -0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,2 +1,10 @@
-from app import app as application
+# wsgi.py — puente robusto
+try:
+    # Caso 1: app.py define "app = Flask(__name__)"
+    from app import app as application
+except ImportError:
+    # Caso 2: app.py define "application = Flask(__name__)" u otro nombre
+    from app import application
+# Gunicorn buscará "app"
 app = application
+
